9716ad2917daca029574050de2d57afd893b052f
abrahamga <abraham.garcia@taxsat.com>
2020-03-22 14:22:33 -0600

    in this moment i 'separe' li of ul to have a better code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { MenuComponent } from './components/menu/menu.component';
 import {PagesModule} from './pages/pages.module';
 import {HttpClientModule} from '@angular/common/http';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
