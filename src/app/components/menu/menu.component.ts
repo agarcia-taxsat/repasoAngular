86f0a6bebc152b3d00327a5d729a1333213b4e74
abrahamga <abraham.garcia@taxsat.com>
2020-03-22 10:14:33 -0600

    if you want to do routerLik with a lot of buttons you must see this

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.sass']
+})
+export class MenuComponent implements OnInit {
+  rutas: any = [
+    {
+      name: 'Home',
+      path: '/home'
+    },
+    {
+      name: 'Contact',
+      path: '/contact'
+    },
+    {
+      name: 'About',
+      path: '/about'
+    },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
