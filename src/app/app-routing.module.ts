351ed54a172e2c9184aed27a032992630054a5a5
abrahamga <abraham.garcia@taxsat.com>
2020-03-22 09:55:02 -0600

    if you want to create a routing for yourself, you must see this. router-outlet

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, ROUTES} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {AboutComponent} from './pages/about/about.component';
+import {ContactComponent} from './pages/contact/contact.component';
+
+
+
+// @ts-ignore
+const routes: ROUTES = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
